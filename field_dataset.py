3fbcb223e270bd5c438eeaec4e125ffe8d678e50
louis <louis.rossignol01@gmail.com>
2024-04-06 19:48:35 +0200

    docstring field_dataset.py

--- field_dataset.py
+++ field_dataset.py
@@ -46,51 +46,28 @@ def get_augmentation(
 
 class FieldDataset(Dataset):
     """
-    A custom Dataset class for handling optical field data in PyTorch, designed to accommodate
-    datasets where each sample includes image data alongside laser power values, and is associated
-    with multiple labels for multi-output modeling tasks. This class facilitates the integration
-    of complex datasets with PyTorch's data handling and model training pipelines, including support
-    for on-the-fly data augmentation during training.
-
-    The class assumes data loading and processing on a specific computing device, which can be specified
-    (e.g., CPU or GPU), and is structured to work with multi-dimensional data that represents
-    different characteristics of optical fields (such as amplitude and phase) and their corresponding
-    physical parameters like laser power and n2 values.
+    A custom dataset class for handling datasets that include features such as 
+    optical field data and labels for various parameters like n2, power, and 
+    saturation intensity (isat), with support for data augmentation during training.
 
     Attributes:
-        device (torch.device): The computing device (CPU or GPU) where the dataset tensors are stored.
-        training (bool): Flag indicating whether the dataset is in training mode, which influences
-                         whether data augmentation is applied.
-        n2_label (torch.Tensor): Tensor containing labels for the nonlinear refractive index values,
-                           used for categorization or parameter estimation.
-        power_label (torch.Tensor): Tensor containing categorical labels for laser power, intended
-                                    for classification tasks.
-        isat_label (torch.Tensor): Tensor containing labels for the saturation index values,
-                           used for categorization or parameter estimation.
-        power_value (torch.Tensor): Tensor containing continuous power values for each sample, useful
-                              for regression tasks.
-        data (torch.Tensor): The primary dataset tensor containing the field data, structured as
-                             [num_samples, num_channels, height, width].
+    - device (torch.device): The computing device where the dataset will be processed.
+    - training (bool): Specifies whether the dataset is used for training, affecting data augmentation.
+    - power_labels (torch.Tensor): Tensor of power labels.
+    - power_values (torch.Tensor): Tensor of continuous power values.
+    - n2_labels (torch.Tensor): Tensor of n2 labels.
+    - isat_labels (torch.Tensor): Tensor of isat labels.
+    - data (torch.Tensor): The main dataset containing the optical field data.
 
-    Initialization Parameters:
-        data (np.ndarray): The dataset containing the optical field data, with shape
-                           [num_samples, num_channels, height, width].
-        power (np.ndarray): One-dimensional array of laser power values, shape [num_samples].
-        power_lab (np.ndarray): One-dimensional array of categorical labels for the laser power,
-                                shape [num_samples].
-        n2 (np.ndarray): One-dimensional array of n2 values, shape [num_samples].
-        training (bool): Indicates if the dataset is initialized in training mode.
-        device (torch.device, optional): Computing device for the tensors, default is CPU.
-
-    Methods:
-        __len__() -> int: Returns the total number of samples in the dataset.
-        __getitem__(idx: int) -> tuple: Retrieves a data sample and its labels by index.
-
-    The dataset class supports indexing for direct access to individual samples, facilitating easy
-    batch loading through PyTorch's DataLoader. It is designed to be flexible, allowing for easy adaptation
-    to various types of optical field data and their associated parameters.
+    Parameters:
+    - data (np.ndarray): Optical field data, shaped as [num_samples, num_channels, height, width].
+    - n2_labels (np.ndarray): Labels for n2 values, one for each data sample.
+    - power_labels (np.ndarray): Labels for power levels, one for each data sample.
+    - isat_labels (np.ndarray): Labels for isat values, one for each data sample.
+    - power_values (np.ndarray): Continuous power values, one for each data sample.
+    - training (bool): Flag to enable data augmentation if True.
+    - device (torch.device): The device to perform computations on, default is CPU.
     """
-    
     def __init__(
             self, 
             data: np.ndarray, 
@@ -101,29 +78,27 @@ class FieldDataset(Dataset):
             training: bool, 
             device=torch.device("cpu")):
         """
-        Initializes an instance of the dataset class, preparing field data and corresponding labels for 
-        use in machine learning models, specifically designed for PyTorch. This includes setting up 
-        tensors for the field data, laser power, n2 values, and applying data augmentation if in training mode.
+        Initializes the FieldDataset instance, setting up the tensors for data and labels, and preparing
+        data augmentation if in training mode.
 
         Parameters:
-        - data (np.ndarray): The dataset containing the optical field data. Expected to be in the format 
-        [num_samples, num_channels, height, width], where 'num_channels' typically corresponds to different 
-        types of data (e.g., amplitude, phase) associated with each field.
-        - power (np.ndarray): A one-dimensional numpy array containing laser power values for each sample in 
-        the dataset, with the shape [num_samples].
-        - power_lab (np.ndarray): A one-dimensional numpy array of categorical labels for the laser power, 
-        intended for classification tasks, with the shape [num_samples].
-        - n2 (np.ndarray): A one-dimensional numpy array containing n2 (nonlinear refractive index) values 
-        associated with each sample, used for categorization or parameter estimation, with the shape [num_samples].
-        - training (bool): Indicates whether the dataset is being initialized for training purposes. When True, 
-        data augmentation is applied to the field data.
-        - device (torch.device, optional): Specifies the computing device where the tensors will be allocated. 
-        Defaults to CPU. Can be set to a CUDA device for GPU acceleration.
+        - data (np.ndarray): The dataset containing the optical field data. The data is expected
+          to be in the format of [num_samples, num_channels, height, width].
+        - n2_labels (np.ndarray): An array of labels for the nonlinear refractive index (n2), 
+          with each label corresponding to a sample in the dataset.
+        - power_labels (np.ndarray): An array of categorical labels for the laser power, 
+          where each label corresponds to a sample.
+        - isat_labels (np.ndarray): An array of labels for the saturation intensities (Isat),
+          with each label corresponding to a sample.
+        - power_values (np.ndarray): An array of continuous values representing the power associated
+          with each sample.
+        - training (bool): A boolean flag that indicates whether the dataset is being used for training.
+          When True, data augmentation is applied.
+        - device (torch.device): The computing device (CPU, GPU) on which the data will be processed and stored.
 
-        This constructor method converts the input numpy arrays into PyTorch tensors and moves them to the specified 
-        computing device. If the instance is marked for training, it applies specified data augmentation techniques 
-        to the field data. The method assumes that a function named 'get_augmentation' is defined elsewhere to obtain 
-        the augmentation procedure based on the data dimensions.
+        This method initializes the dataset by converting numpy arrays into PyTorch tensors and moving
+        them to the specified device. It also prepares the data augmentation process if the dataset is
+        initialized for training purposes.
         """
         self.device = device
         self.training = training
@@ -150,45 +125,25 @@ class FieldDataset(Dataset):
     
     def __len__(self) -> int:
         """
-        Returns the total number of samples in the dataset.
-
-        This method is a required implementation for any PyTorch dataset class. It allows PyTorch's 
-        DataLoader to know the size of the dataset for batching purposes.
+        Returns the number of samples in the dataset.
 
         Returns:
-        int: The number of samples in the dataset.
+        int: The total number of samples in the dataset.
         """
         return len(self.data)
 
     def __getitem__(self, idx: int) -> tuple:
         """
-        Retrieves a data sample and its associated labels by index.
-
-        This method is crucial for PyTorch's DataLoader to iterate over the dataset. It allows direct 
-        access to data points and their labels, making it straightforward to load batches of data 
-        during the training or evaluation of models.
+        Retrieves a single data sample and its associated labels by index.
 
         Parameters:
-        - idx (int): The index of the data item to be retrieved. This should be within the range 
-        of 0 to the length of the dataset minus one.
+        - idx (int): Index of the data sample to retrieve.
 
         Returns:
-        tuple: A tuple containing the data item and its associated labels. Specifically, the tuple 
-        structure is (data_item, power_value, power_labels, n2_label), where:
-            - data_item (torch.Tensor): The field data for the given index, maintaining the shape 
-            as defined during the dataset initialization (e.g., [num_channels, height, width]).
-            - power_value (torch.Tensor): The continuous power value associated with this data item, 
-            useful for regression tasks.
-            - power_labels (torch.Tensor): The categorical label for the power, intended for 
-            classification tasks.
-            - n2_label (torch.Tensor): The label for the nonlinear refractive index (n2) value, 
-            which could be used for categorization or parameter estimation.
-
-        This method supports indexing into the dataset to retrieve individual samples along with 
-        their corresponding labels, formatted as PyTorch tensors. The tensors are already moved to 
-        the appropriate device (e.g., CPU, GPU) as specified during the dataset's initialization.
+        tuple: A tuple containing the data sample and its labels (data_item, power_value, power_label, n2_label, isat_label),
+               where `data_item` is the optical field data, `power_value` is the continuous power value, and
+               `power_label`, `n2_label`, `isat_label` are the respective labels for the sample.
         """
-
         data_item = self.data[idx,:,:,:]
         power_value =self.power_values[idx]
         power_label =self.power_labels[idx]
