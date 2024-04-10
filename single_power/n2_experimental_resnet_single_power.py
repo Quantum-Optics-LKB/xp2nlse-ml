96151a4ee274043d7c3118361ee2df5885ecb9cb
louis <louis.rossignol01@gmail.com>
2024-04-10 13:06:19 +0200

    final test

--- single_power/n2_experimental_resnet_single_power.py
+++ single_power/n2_experimental_resnet_single_power.py
@@ -9,14 +9,15 @@ from skimage.restoration import unwrap_phase
 from engine.nlse_generator import normalize_data
 
 path = "/home/louis/LEON/DATA/Atoms/2024/PINNS2/CNN"
-resolution = 512
 number_of_n2 = 10
-number_of_puiss = 10
+number_of_power = 10
+number_of_isat = 10
 batch_size = 20
 learning_rate = 0.001
-puiss_label_clean = np.arange(0, number_of_puiss)
-puiss_value_clean = np.linspace(0.02, .5001, number_of_puiss)
-puiss_value_normed = (puiss_value_clean - np.min(puiss_value_clean) ) / (np.max(puiss_value_clean) - np.min(puiss_value_clean))
+power_labels = np.arange(0, number_of_power)
+power_values = np.linspace(0.02, .5001, number_of_power)
+
+resolution_out = 256
 
 backend = "GPU"
 if backend == "GPU":
@@ -26,112 +27,98 @@ else:
 print("---- DATA LOADING ----")
 
 E = np.load("/home/louis/LEON/DATA/Atoms/2024/PINNS2/CNN/exp_data/field.npy")[:,0,:,:]
-E_data = np.zeros((E.shape[0], 2, E.shape[1], E.shape[2]))
-
-E_data[:,0,:, :] = np.abs(E)**2
-out_amp = np.abs(E)**2
-E[out_amp < 50000] = 0
-E_data[:,1,:, :] = np.angle(E)
+E_data = np.zeros((E.shape[0], 3, E.shape[1], E.shape[2]))
 
 cut = (E.shape[2] - E.shape[1])//2
 E_reshape = E[:,:,cut:E.shape[2] - cut] 
-E_resized =  zoom(E_reshape, (1, 512/E_reshape.shape[1],512/E_reshape.shape[2]), order=3)
+E_resized =  zoom(E_reshape, (1, resolution_out/E_reshape.shape[1],resolution_out/E_reshape.shape[2]), order=3)
 
-E_amp_pha_unwrap = np.zeros((E.shape[0], 2, 512, 512))
-E_amp_pha_unwrap[:,0,:,:] = np.abs(E_resized)**2
-E_amp_pha_unwrap[:,1,:,:] = unwrap_phase(np.angle(E_resized))
-E_amp_pha_unwrap = normalize_data(E_amp_pha_unwrap)
-
-E_amp_pha = np.zeros((E.shape[0], 2, 512, 512))
-E_amp_pha[:,0,:,:] = np.abs(E_resized)**2
-E_amp_pha[:,1,:,:] = np.angle(E_resized)
-E_amp_pha = normalize_data(E_amp_pha)
-
-# # Normalize the data
-# E_norm = np.roll(normalize_data(E_data), (-15,10), axis=(2,3))
-# E_norm[:,:,256,256] = 1
+E = np.zeros((E.shape[0], 3, resolution_out, resolution_out))
+E[:,0,:,:] = np.abs(E_resized)**2
+E[:,1,:,:] = np.angle(E_resized)
+E[:,2,:,:] = unwrap_phase(np.angle(E_resized))
+E= normalize_data(E)
 
 
-for i in range(0, 10):
-    plt.figure(figsize=(10,10 ))
-    plt.imshow(E_data[i,0,:,:], cmap='viridis')
-    plt.savefig(f"{path}/old_data_{i}_density.png")
-    plt.close()
+classes = {
+        'n2': tuple(map(str, np.linspace(-1e-9, -1e-10, number_of_n2))),
+        'power' : tuple(map(str, np.linspace(0.02, .5001, number_of_power))),
+        'isat' : tuple(map(str, np.linspace(1e4, 1e6, number_of_isat) ))
+    }
 
-    plt.figure(figsize=(10,10 ))
-    plt.imshow(E_data[i,1,:,:], cmap='viridis')
-    plt.savefig(f"{path}/old_data_{i}_phase.png")
-    plt.close()
+field_data = [E[:,0,:,:], E[:,[0,1],:,:], E[:,[0,2],:,:], E[:,1,:,:], E[:,2,:,:], E]
+data_types = ["amp", "amp_pha", "amp_pha_unwrap", "pha", "pha_unwrap", "amp_pha_pha_unwrap"]
 
-# field_data = [E_amp_pha[:,[0],:,:], E_amp_pha, E_amp_pha_unwrap, E_amp_pha[:,[1],:,:],E_amp_pha_unwrap[:,[1],:,:]]
-# classes = {
-#         'n2': tuple(map(str, np.linspace(-1e-9, -1e-10, number_of_n2))),
-#         'power' : tuple(map(str, np.linspace(0.02, .5001, number_of_puiss)))
-#     }
+data_types_results = {}
+for data_types_index in range(len(data_types)):
 
-# data_types = ["amp", "amp_pha", "amp_pha_unwrap", "pha", "pha_unwrap"]
-# noise_types = ["noise","no_noise"]
+    E = field_data[data_types_index]
 
-# results_index_accuracy = np.zeros((2, 5))
-# results_index_std = np.zeros((2, 5))
+    models = {}
+    for model_index in range(2, 6):
+            
+        if model_index == 2:
+            from single_power.model.model_resnetv2_1powers import Inception_ResNetv2
+        elif model_index == 3:
+            from single_power.model.model_resnetv3_1powers import Inception_ResNetv2
+        elif model_index == 4:
+            from single_power.model.model_resnetv4_1powers import Inception_ResNetv2
+        elif model_index == 5:
+            from single_power.model.model_resnetv5_1powers import Inception_ResNetv2
+        
+        model_version =  str(Inception_ResNetv2).split('.')[-2]
+        
+        result_n2 = np.zeros(number_of_power)
+        result_index_n2 = np.zeros(number_of_power)
 
-# results_n2_accuracy = np.zeros((2, 5))
-# results_n2_std = np.zeros((2, 5))
-# for noise_index in range(2):
+        result_isat = np.zeros(number_of_power)
+        result_index_isat = np.zeros(number_of_power)
+        
+        for power_index in power_labels:
+            
+            stamp = f"power{str(power_values[power_index])[:4]}_{data_types[data_types_index]}_{model_version}"
+            new_path = f"{path}/{stamp}_training"
 
-#     for data_types_index in range(5):
-#         print(data_types_index)
+            
+            cnn = Inception_ResNetv2(in_channels=E.shape[1], class_n2=number_of_n2, class_power=number_of_power, class_isat=number_of_isat)
+            cnn = cnn.to(device)
+            cnn.load_state_dict(torch.load(f'{new_path}/n2_net_w{resolution_out}_n2{number_of_n2}_isat{number_of_isat}_puiss{1}.pth'))
 
-#         E = field_data[data_types_index]
-
-#         for model_index in [3]:#range(2, 6):
-                
-#                 if model_index == 2:
-#                     from model_resnetv2_1powers import Inception_ResNetv2
-#                 elif model_index == 3:
-#                     from model_resnetv3_1powers import Inception_ResNetv2
-#                 elif model_index == 4:
-#                     from model_resnetv4_1powers import Inception_ResNetv2
-#                 elif model_index == 5:
-#                     from model_resnetv5_1powers import Inception_ResNetv2
-                
-#                 model_version =  str(Inception_ResNetv2).split('.')[0][8:]
-                
-#                 result = np.zeros(10)
-#                 result_index = np.zeros(10)
-#                 for puiss_index in range(10):
+            with torch.no_grad():
+                images = torch.from_numpy(E[[power_index],:,:,:]).float().to(device)
+                labels_power = torch.from_numpy(power_labels[[power_index],]).long().to(device)
+                powers_values = torch.from_numpy(power_values[[power_index],np.newaxis]).float().to(device)
                     
-#                     stamp = f"{noise_types[noise_index]}_power{str(puiss_value_clean[puiss_index])[:4]}_{data_types[data_types_index]}_{model_version}"
-                    
-#                     new_path = f"{path}/{stamp}_training"
+                outputs_n2, outputs_isat = cnn(images, powers_values)
+                _, predicted_n2 = torch.max(outputs_n2, 1)
+                _, predicted_isat = torch.max(outputs_isat, 1)
 
-                    
-#                     cnn = Inception_ResNetv2(in_channels=E.shape[1], batch_size=batch_size,class_n2=number_of_n2, class_power=number_of_puiss)
-#                     cnn = cnn.to(device)
+                result_n2[power_index] = classes['n2'][predicted_n2]
+                result_index_n2[power_index] = predicted_n2
 
-#                     cnn.load_state_dict(torch.load(f'{new_path}/n2_net_w{resolution}_n2{number_of_n2}_puiss{1}_2D.pth'))
-
-#                     with torch.no_grad():
-#                         images = torch.from_numpy(E[[puiss_index],:,:,:]).float().to(device)
-#                         labels_power = torch.from_numpy(puiss_label_clean[[puiss_index],]).long().to(device)
-#                         powers_values = torch.from_numpy(puiss_value_normed[[puiss_index],np.newaxis]).float().to(device)
-                            
-                    
-#                         outputs_n2 = cnn(images, powers_values)
-#                         _, predicted_n2 = torch.max(outputs_n2, 1)
-
-#                         result[puiss_index] = classes['n2'][predicted_n2]
-#                         result_index[puiss_index] = predicted_n2
-                    
-                    
-#                 results_index_accuracy[noise_index, data_types_index] = np.mean(result)
-#                 results_n2_std[noise_index, data_types_index] = np.std(result)
-#                 results_index_accuracy[noise_index, data_types_index] = np.mean(result_index)
-#                 results_index_std[noise_index, data_types_index] = np.std(result_index)
+                result_isat[power_index] = classes['isat'][predicted_isat]
+                result_index_isat[power_index] = predicted_isat
+        
+        models[model_version] = [result_n2, result_index_n2, result_isat, result_index_isat]
+    data_types_results[data_types[data_types_index]] = models
 
 
+for data_types_index in range(len(data_types)):
+    for model_index in range(2, 6):
+        result_n2 = np.array(data_types_results[data_types[data_types_index]][f"model_resnetv{model_index}_1powers"][0])
+        result_index_n2 = np.array(data_types_results[data_types[data_types_index]][f"model_resnetv{model_index}_1powers"][1])
+        result_isat = np.array(data_types_results[data_types[data_types_index]][f"model_resnetv{model_index}_1powers"][2])
+        result_index_isat = np.array(data_types_results[data_types[data_types_index]][f"model_resnetv{model_index}_1powers"][3])
+        
+        print(f"For {data_types[data_types_index]} and model_resnetv{model_index}_1powers :\n") 
+        print(f"Average n2 = {np.mean(result_n2)}")
+        print(f"Std n2 = {np.std(result_n2)}")
 
-# print(results_index_accuracy)
-# print(results_index_std)
-# print(results_n2_accuracy)
-# print(results_n2_std)               
+        print(f"Average index n2 = {np.mean(result_index_n2)}")
+        print(f"Std index n2 = {np.std(result_index_n2)}")
+
+        print(f"Average isat = {np.mean(result_isat)}")
+        print(f"Std isat = {np.std(result_isat)}")
+
+        print(f"Average index isat = {np.mean(result_index_isat)}")
+        print(f"Std index isat = {np.std(result_index_isat)}")
