2aa8f069cbf03a5b18d1b4c17233898378e4cc24
louis <louis.rossignol01@gmail.com>
2024-06-27 17:37:44 +0200

    parameters

--- parameters.py
+++ parameters.py
@@ -15,14 +15,15 @@ generate = False
 create_visual = False
 
 ###Parameter spaces:
-number_of_n2 = 30
-number_of_isat = 30
-n2 = -5*np.logspace(-10, -9, number_of_n2) #m/W^2 [-5e-10 -> -5e-9]
-isat = np.logspace(4, 5, number_of_isat) #W/m^2 [1e4 -> 1e5]
+number_of_n2 = 5
+number_of_isat = 5
+number_of_alpha = 5
+n2 = -np.logspace(-9, -8, number_of_n2) #m/W^2 [-1e-9 -> -1e-8]
+isat = np.logspace(3, 6, number_of_isat) #W/m^2 [1e3 -> 1e6]
+alpha = np.linspace(10, 60, number_of_alpha) #m^-1 [10 -> 60]
 
 ###Laser Parameters:
 input_power = 1.05 #W
-alpha = 22 #m^-1
 waist_input_beam = 2.3e-3 #m
 
 ###Training Parameters:
@@ -34,6 +35,6 @@ use=True
 plot_generate_compare=True
 
 manager(generate, training, create_visual, use, plot_generate_compare,
-         window_out, n2, number_of_n2, alpha, isat, number_of_isat, 
+         window_out, n2, number_of_n2, alpha, number_of_alpha, isat, number_of_isat, 
          input_power, waist_input_beam, cell_length, 
          saving_path, exp_image_path)
